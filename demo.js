202357ea7c1439e4c821b0e9abdaaaa2d273ad78
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-01-05 11:34:29 -0800

    Simplify demo script

--- demo.js
+++ demo.js
@@ -1,8 +1,13 @@
 const { CaptureInterface } = require("./lib/pcap-ffxiv");
 
 const ci = new CaptureInterface();
+
 ci.on("message", (type, message) => {
     console.log(type);
     console.log(message.parsedIpcData);
 })
-ci.on("ready", () => ci.open(process.argv[2]));
+
+// Get the first device with an IPv4 address.
+const device = ci.getDevices().find(d => d.addresses.some(a => !a.addr.includes("::")));
+
+ci.on("ready", () => ci.open(device.addresses[0].addr));
