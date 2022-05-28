8455b414f8fda7819fd3057a9ace72d3ab22b583
Jade <jade@witchof.space>
2022-05-28 18:14:35 -0400

    Capitalization fixed

--- src/packet-processors/processors/desynthResult.ts
+++ src/packet-processors/processors/desynthResult.ts
@@ -2,14 +2,14 @@ import { BufferReader } from "../../BufferReader";
 import { DesynthResult } from "../../definitions";
 
 export function desynthResult(reader: BufferReader): DesynthResult {
-	const unknown00 = reader.nextUint32();
-	const unknown01 = reader.nextUint32();
-	const itemResult = reader.nextUint32();
-	return {
+	const unknown00 = reader.nextUInt32();
+  const unknown01 = reader.nextUInt32();
+  const itemResult = reader.nextUInt32();
+  return {
 		unknown0: unknown00, 
 		unknown1: unknown01,
 		itemId: itemResult % 1000000,
-		itemHq: itemResult > 1000000,
+    itemHq: itemResult > 1000000,
 		result: Array(3)
 			.fill(null)
 			.map(() => {
