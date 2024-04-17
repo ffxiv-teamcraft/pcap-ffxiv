b6216f932e42cb9de39d08bd360d59b1d555f2b5
Flavien Normand <contact@flavien-normand.fr>
2024-04-17 23:30:41 +0200

    feat(opcodes): better fallback logic

--- src/json-downloader.ts
+++ src/json-downloader.ts
@@ -1,12 +1,21 @@
 import https from "https";
 
 export function downloadJson<T>(uri: string): Promise<T> {
-	return new Promise((resolve) => {
+	return new Promise((resolve, reject) => {
 		const req = https.request(new URL(uri), (res) => {
 			const data: any[] = [];
 			res.setEncoding("utf8");
 			res.on("data", (chunk) => data.push(chunk));
-			res.on("end", () => resolve(JSON.parse(data.join(""))));
+			res.on("end", () => {
+				try {
+					resolve(JSON.parse(data.join("")));
+				} catch (e) {
+					reject(e);
+				}
+			});
+		});
+		req.on("error", err => {
+			reject(err);
 		});
 		req.end();
 	});
