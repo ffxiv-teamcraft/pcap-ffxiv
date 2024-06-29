19914852b9cad83e0858b5bd82c9ffe169ea4e54
Flavien Normand <contact@flavien-normand.fr>
2024-06-29 10:55:25 +0200

    fix(KR/CN): bring back older processors based on region

--- src/packet-processors/processors/playerSetup.ts
+++ src/packet-processors/processors/playerSetup.ts
@@ -1,7 +1,31 @@
 import { BufferReader } from "../../BufferReader";
 import { PlayerSetup } from "../../definitions";
+import { ConstantsList, Region } from "../../models";
 
-export function playerSetup(reader: BufferReader): PlayerSetup {
+export function playerSetup(reader: BufferReader, constants?: ConstantsList, region?: Region): PlayerSetup {
+	if(region !== 'Global'){
+		return {
+			contentId: reader.nextUInt64(),
+			unknown8: reader.nextUInt32(),
+			unknownC: reader.nextUInt32(),
+			charId: reader.nextUInt32(),
+			restedExp: reader.nextUInt32(),
+			companionCurrentExp: reader.nextUInt32(),
+			unknown1C: reader.nextUInt32(),
+			fishCaught: reader.nextUInt32(),
+			useBaitCatalogId: reader.nextUInt32(),
+			unknown28: reader.nextUInt32(),
+			unknownPvp2C: reader.nextUInt32(),
+			unknown3: reader.nextUInt32(),
+			pvpFrontlineOverallCampaigns: reader.nextUInt32(),
+			unknownTimestamp34: reader.nextUInt32(),
+			unknownTimestamp38: reader.nextUInt32(),
+			name: reader
+				.slice(0x2a2, 0x2c2)
+				.toString("utf8")
+				.replace(/^[\s\uFEFF\xA0\0]+|[\s\uFEFF\xA0\0]+$/g, ""),
+		};
+	}
 	return {
 		contentId: reader.nextUInt64(),
 		unknown8: reader.nextUInt32(),
@@ -19,7 +43,7 @@ export function playerSetup(reader: BufferReader): PlayerSetup {
 		unknownTimestamp34: reader.nextUInt32(),
 		unknownTimestamp38: reader.nextUInt32(),
 		name: reader
-			.slice(0x2F8, 0x318)
+			.slice(0x2f8, 0x318)
 			.toString("utf8")
 			.replace(/^[\s\uFEFF\xA0\0]+|[\s\uFEFF\xA0\0]+$/g, ""),
 	};
